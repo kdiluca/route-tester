bff7daccd918ed9755a6dc48bad27e0d61cbfac0
kdiluca <kristen@mapzen.com>
2015-06-05 10:19:20 -0400

    updated to latest date time updates

--- js/valhalla.js
+++ js/valhalla.js
@@ -184,21 +184,82 @@ var getEndIcon = function(icon){
   var bikeBtn = document.getElementById("bike_btn");
   var walkBtn = document.getElementById("walk_btn");
   var multiBtn = document.getElementById("multi_btn");
+  var timeBtn = document.getElementById("time_btn");
+  
+  var time = new Date();
+  var day = time.getDate();
+  if (day < 10) {
+    day = '0' + day;
+  }
+  var month = time.getMonth() + 1;
+  if (month < 10) {
+    month = '0' + month;
+  }
+  var year = time.getFullYear();
+  var hour = 12;
+  var minute = 15;
+  dateStr = year + "-" + month + "-" + day + "T" + hour + ":" + minute;
   
 driveBtn.addEventListener('click', function (e) {
+    // var newWayPoints = new L.Routing.Waypoint([
+    //     L.latLng(39.645244,-73.9449975),
+    //     L.latLng(39.7590615,-73.969231)
+    // ]);
+    // rr.setWaypoints(newWayPoints);
   rr.route({transitmode: 'auto'});
 });
 
 bikeBtn.addEventListener('click', function (e) {
   rr.route({transitmode: 'bicycle'});
 });
+
 walkBtn.addEventListener('click', function (e) {
   rr.route({transitmode: 'pedestrian'});
 }); 
 
 multiBtn.addEventListener('click', function (e) {
-  rr.route({transitmode: 'multimodal'});
+	  rr.route({transitmode: 'multimodal', date_time: dateStr});
 });
+
+timeBtn.addEventListener('click', function (e) {
+	  var newTime = prompt("Set time/date to depart (24hr clock), hh:mm MM-DD");
+	  if (newTime.length >= 5) {
+	    var newHour = newTime.substring(0,2);
+	    hour = (newHour <= 24 && newHour >= 0) ? newHour : hour;
+	    var newMin = newTime.substring(3,5);
+	    minute = (newMin < 60  && newMin >= 0) ? newMin : minute;
+	    if (newTime.length === 11) {
+	      var mdStr = newTime.substring(6, newTime.length);
+	      var newMonth = mdStr.substring(0,2);
+	      var newDay = mdStr.substring(3,5);
+	      if (newMonth > 0 && newMonth <= 12) {
+	        if (newMonth < time.getMonth() + 1) {
+	          year++;
+	        } else {
+	          year = time.getFullYear();
+	        }
+	        month = newMonth;
+	        if (newDay > 0 && newDay <= 31) {
+	          day = newDay;
+	        }
+	      }   
+	    }
+	  } else if (newTime === "r") {
+	    year = time.getFullYear();
+	    month = time.getMonth() + 1;
+	    if (month < 10) {
+	      month = '0' + month;
+	    }
+	    day = time.getDate();
+	    if (day < 10) {
+	      day = '0' + day;
+	    }
+	    hour = 12;
+	    minute = 15;
+	  } 
+	  dateStr = year + "-" + month + "-" + day + "T" + hour + ":" + minute;
+	  multiBtn.click();
+	});
 
 $(document).on('mode-alert', function(e, m) {
     mode = m;
