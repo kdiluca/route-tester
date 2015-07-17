e7b5c5ea2e0f98dadeed7a5ee8fd3a4fdf90733d
kdiluca <kristen@mapzen.com>
2015-07-17 12:34:13 -0400

    fixed route file appending bug, added ability to load log files, and now running routes

--- js/valhalla.js
+++ js/valhalla.js
@@ -40,37 +40,46 @@ var inputElement = document.getElementById("inputFile");
 inputElement.addEventListener("change", selectFiles, false);
 
 function selectFiles(evt) {
-  var files = evt.target.files;
-  if (!files.length) {
-    alert('Please select a file!');
-    return;
+  if (typeof evt.target != "undefined") {
+	  var files =  evt.target.files;
+	  
+	  if (!files.length) {
+	    alert('Please select a file!');
+	    return;
+	  }
+	  var file = files[0];
+	  var reader = new FileReader();
+	  var delimiter = "-j";
+	  reader.onloadend = function(evt) {
+	    if (evt.target.readyState == FileReader.DONE) {
+	      var lines = evt.target.result.split(delimiter);
+	      var output = "";
+	      var index;
+		  var select = document.getElementById('selector').options.length = 0;
+	      for (index = 0; index < lines.length; index++) {
+	    	  //increment to 1 if a test file (split on -j)
+	    	  if (lines[0]=="")
+	    		  index++;
+	    	  var newOption = document.createElement('option');
+	          var pattern = new RegExp("{\".*}", "g");
+	          var results = pattern.exec(unescape(lines[index]));
+	          lines[index] = results[0];
+	          newOption.text = lines[index];
+	          newOption.value = index;
+	          //reset selector options
+	          select = document.getElementById('selector');
+	          try {
+	            select.add(newOption, null);
+	          } catch (ex) {
+	            select.add(newOption);
+	          }
+	        }
+	    }
+	  };
+	  reader.readAsText(file);
   }
-  var file = files[0];
-  var reader = new FileReader();
-  var delimiter = "-j";
-  reader.onloadend = function(evt) {
-    if (evt.target.readyState == FileReader.DONE) {
-      var lines = evt.target.result.split(delimiter);
-      var output = "";
-      var index;
-      for (index = 1; index < lines.length; index++) {
-    	  var newOption = document.createElement('option');
-          var pattern = new RegExp("{\".*}}", "g");
-          var results = pattern.exec(lines[index]);
-          lines[index] = results[0];
-          newOption.text = lines[index];
-          newOption.value = index;
-          var select = document.getElementById('selector');
-          try {
-            select.add(newOption, null);
-          } catch (ex) {
-            select.add(newOption);
-          }
-        }
-    }
-  };
-  reader.readAsText(file);
 }
+
 
 app.run(function($rootScope) {
 	  var hash_loc = hash_params ? hash_params : {'center': {'lat': 40.7486, 'lng': -73.9690}, 'zoom': 13};
@@ -135,6 +144,25 @@ app.controller('RouteController', function($scope, $rootScope, $sce, $http) {
 	  });
 	};
 
+	var getFileOriginIcon = function(icon){
+	  return L.icon({
+	    iconUrl: 'resource/start_greendot.png',
+
+	   // iconSize:     [44, 56], // size of the icon
+	   // iconAnchor: [22, 50]
+
+	  });
+	};
+	
+	var getFileDestIcon = function(icon){
+	  return L.icon({
+	    iconUrl: 'resource/destmarker@2x.png',
+	   // iconSize:   [44, 56], // size of the icon
+	   // iconAnchor: [22, 50]
+	  });
+	};
+
+	//TODO:  Switch out maps based on travel mode
 	L.tileLayer('http://{s}.tile.thunderforest.com/transport/{z}/{x}/{y}.png', {
 	    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a>',
 	    maxZoom: 18
@@ -178,6 +206,32 @@ app.controller('RouteController', function($scope, $rootScope, $sce, $http) {
 	    //marker.openPopup();
 	  });
 	  
+	  $rootScope.$on( 'map.dropOriginMarker', function( ev, geo, m){
+
+	    if (locations == 0) {
+	      var marker = new L.marker(geo, {icon: getFileOriginIcon(m || 'car')});
+	    }
+	    else {
+	      var marker = new L.marker(geo, {icon: getFileOriginIcon(m || 'car')});
+	    }
+	    map.addLayer(marker);
+	    markers.push(marker);
+	    //marker.openPopup();
+	  });
+	  
+	  $rootScope.$on( 'map.dropDestMarker', function( ev, geo, m){
+
+		    if (locations == 0) {
+		      var marker = new L.marker(geo, {icon: getFileDestIcon(m || 'car')});
+		    }
+		    else {
+		      var marker = new L.marker(geo, {icon: getFileDestIcon(m || 'car')});
+		    }
+		    map.addLayer(marker);
+		    markers.push(marker);
+		    //marker.openPopup();
+		  });
+		  
 	  $scope.renderHtml = function(html_code){
 	    return $sce.trustAsHtml(html_code);
 	  };
@@ -193,6 +247,161 @@ app.controller('RouteController', function($scope, $rootScope, $sce, $http) {
 	      $scope.route_instructions = '';
 	    });
 	  });
+	  
+	  document.querySelector(".select").addEventListener('click',
+			  function(evt) {
+			 //   if (e.target.tagName.toLowerCase() == 'button') {
+			      var select = document.getElementById('selector');
+			      var i;
+			      for (i = 0; i < select.length; i++) {
+			        if (select.options[i].selected) {
+				    	Locations = [];
+					    reset();
+			        	var json = JSON.parse(select.options[i].innerHTML);
+			        	var mode = json.costing;
+			        	var options = json.directions_options;
+			        	
+			        	if (json.locations.length == 2) {
+			        		var geo = {
+			    	          'olat' : json.locations[0].lat,
+			    	          'olon' : json.locations[0].lon,
+			    	          'dlat' : json.locations[1].lat,
+			    	          'dlon' : json.locations[1].lon
+				        	}
+				        if (json.locations == 2) {
+				          Locations.push({lat: geo.olat, lon: geo.olon })
+					   //   $rootScope.$emit( 'map.dropOriginMarker', [geo.olat, geo.olon], mode);
+				          Locations.push({lat: geo.dlat, lon: geo.dlon })
+						//  $rootScope.$emit( 'map.dropDestMarker', [geo.dlat, geo.dlon], mode);
+					      //locations++;
+					      return;
+					    } else if (json.locations > 2) {
+				    	  Locations = [];
+					      reset();
+					      /*Locations.push({lat: geo.lat, lon: geo.lon })
+					      $rootScope.$emit( 'map.dropFileMarker', [geo.lat, geo.lon], mode);
+					      locations++;*/
+					      return;
+					    }
+			        	
+					    var waypoints = [];
+					    Locations.forEach(function(gLoc) {
+					      waypoints.push(L.latLng(gLoc.lat, gLoc.lon));
+					    });
+
+					    waypoints.push(L.latLng(geo.olat, geo.olon));
+					    waypoints.push(L.latLng(geo.dlat, geo.dlon));
+
+					   // $rootScope.$emit( 'map.dropOriginMarker', [geo.olat, geo.olon], mode);
+					   // $rootScope.$emit( 'map.dropDestMarker', [geo.dlat, geo.dlon], mode);
+					    locations++;
+			        	}
+					    valhalla_mode = mode_mapping[mode];
+
+					var rr = L.Routing.control({
+					  waypoints: waypoints,
+					  geocoder: null,
+					  transitmode: valhalla_mode,
+					  routeWhileDragging: false,
+					  router: L.Routing.valhalla(envToken,'auto'),
+					  summaryTemplate:'<div class="start">{name}</div><div class="info {transitmode}">{distance}, {time}</div>',
+					  
+					  createMarker: function(i,wp,n){
+				      var iconV;
+				        if(i == 0){
+				          iconV = L.icon({
+				          iconUrl: 'resource/start_green_dot.gif',
+				          iconSize:[24,24]
+				          });
+				        }else{
+				          iconV = L.icon({
+				          iconUrl: 'resource/dest_red_dot.png',
+				          iconSize:[24,24]
+				        })
+				        }
+				        var options = {
+				          draggable: true,
+				          icon: iconV
+				        }
+				        return L.marker(wp.latLng,options);
+					  },
+					  formatter: new L.Routing.Valhalla.Formatter(),
+					    pointMarkerStyle: {radius: 6,color: '#25A5FA',fillColor: '#5E6472',opacity: 1,fillOpacity: 1}
+						}).addTo(map);
+					
+				  var driveBtn = document.getElementById("drive_btn");
+				  var bikeBtn = document.getElementById("bike_btn");
+				  var walkBtn = document.getElementById("walk_btn");
+				  var multiBtn = document.getElementById("multi_btn");
+				  var datetime = document.getElementById("datetimepicker");
+				  
+				  driveBtn.addEventListener('click', function (e) {
+					getEnvToken();
+				    rr.route({transitmode: 'auto'});
+				  });
+
+				  bikeBtn.addEventListener('click', function (e) {
+					getEnvToken();
+				    rr.route({transitmode: 'bicycle'});
+				  });
+
+				  walkBtn.addEventListener('click', function (e) {
+					getEnvToken();
+				    rr.route({transitmode: 'pedestrian'});
+				  }); 
+
+				  multiBtn.addEventListener('click', function (e) {
+					getEnvToken();
+				    rr.route({transitmode: 'multimodal', date_time: dateStr});
+				  });
+
+				  function datetimeUpdate(datetime) {
+				      var changeDt = datetime;
+				      var inputDate, splitDate, year, month, day, time, hour, minute; 
+				       if(changeDt != null){
+				   	     if (changeDt.length >= 11) {
+				   	    	inputDate = changeDt.split(" ");
+				   	    	splitDate = inputDate[0].split("-");
+				     	    day = splitDate[0];
+				     	    if (day < 10) {
+				      	      day = '0' + day;
+				      	    } 
+				     	    month = GetMonthIndex(splitDate[1])+1;
+				     	   if (month < 10) {
+				     		  month = '0' + month;
+				       	    } 
+				     	    year = splitDate[2];
+				     	  
+				    	  time = inputDate[1].split(":");
+				     	  hour = time[0];
+				     	  minute = time[1];
+				   	      
+				   	      dateStr = year + "-" + month + "-" + day + "T" + hour + ":" + minute;
+				   	    } else {
+				   		    dateStr = parseIsoDateTime(isoDateTime.toString());
+				   	    }
+				   	    multiBtn.click();	
+				       }
+
+				  };
+
+				  $(document).on('mode-alert', function(e, m) {
+				    mode = m;
+				    reset();
+				    Locations = [];
+				  });
+
+				  $(document).on('route:time_distance', function(e, td){
+				    var instructions = $('.leaflet-routing-container.leaflet-control').html();
+				    $scope.$emit( 'setRouteInstruction', instructions);
+				  });
+
+				  $("#datepicker").on("click", function() {
+			    		datetimeUpdate(this.value);
+			    	  });
+			    }
+			  }
+				  }, false); 
 
 	  map.on('click', function(e) {
 	    var geo = {
@@ -337,18 +546,7 @@ app.controller('RouteController', function($scope, $rootScope, $sce, $http) {
     document.getElementById('inputFile').addEventListener('change', selectFiles, false);
   });
   
-  document.querySelector('.select').addEventListener('click',
-		  function(evt) {
-		 //   if (evt.target.tagName.toLowerCase() == 'button') {
-		      var select = document.getElementById('selector');
-		      var i;
-		      for (i = 0; i < select.length; i++) {
-		        if (select.options[i].selected) {
-		         //TODO: Run route selected
-		        }
-		      }
-		 //   }
-		  }, false);
-  
   });
+	    
+	  
 })
